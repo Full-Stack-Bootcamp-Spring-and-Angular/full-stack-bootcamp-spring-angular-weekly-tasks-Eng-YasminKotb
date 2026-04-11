6e8da7036af067e2abfb0278854bf1f91733ed99
Eng-YasminKotb <ysysys141516@gmail.com>
2026-04-11 18:34:52 +0200

    feat: add logic layer

---
+++ products-management/src/main/java/com/app/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.app.service;
+
+import com.app.entity.Product;
+import com.app.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+    public Product getById(int id) {
+        return productRepository.getById(id);
+    }
+
+    public void delete(int id) {
+        productRepository.deleteById(id);
+    }
+}
